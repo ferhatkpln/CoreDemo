589dda2fd491697737b2620efceedf548e8e74cc
ferhatkpln <kaplanfrht123@gmail.com>
2021-11-22 17:16:06 +0300

    41 ders bitti.

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDAL _aboutDal;
+
+        public AboutManager(IAboutDAL aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public List<About> GetList()
+        {
+            return _aboutDal.GetListAll();
+        }
+    }
+}
--- CoreDemo/Startup.cs
+++ CoreDemo/Startup.cs
@@ -51,7 +51,7 @@ namespace CoreDemo
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Blog}/{action=Index}/{id?}");
             });
         }
     }
